20d5bf141d867260c99f06de789c3d54d1ca05dd
vinanu123 <44706596+vinanu123@users.noreply.github.com>
2019-02-27 13:08:46 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('videoElement') videoElement: any;
+  video: any;
+  isshow: boolean;
+  isPlaying = false;
+
+  displayControls = true;
+
+ constructor(){
+    this.isshow = false;
+  }
+  showcam() {
+    this.isshow = true;
+  }
+
+
+
+
+  pause() {
+    this.video.pause();
+  }
+
+  toggleControls() {
+    this.video.controls = this.displayControls;
+    this.displayControls = !this.displayControls;
+  }
+
+  resume() {
+    this.video.play();
+  }
+
+
+}
